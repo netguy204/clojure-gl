25468898cc79781dc8ce4c155cbe93902a8e8ff3
Brian Taylor <el.wubo@gmail.com>
2012-01-20 20:25:01 -0500

    textures always seemed harder than they aught to be

--- src/clojure_gl/core.clj
+++ src/clojure_gl/core.clj
@@ -1,5 +1,5 @@
 (ns clojure-gl.core
-  (:use (clojure-gl prepare))
+  (:use (clojure-gl prepare texture))
   (:import (org.lwjgl LWJGLException)
            (org.lwjgl.opengl Display GL11)))
 
@@ -11,9 +11,21 @@
 (def ^:dynamic *width* nil)
 (def ^:dynamic *height* nil)
 (def ^:dynamic *aspect-ratio* nil)
+(def ^:dynamic *texture-cache* nil)
+(def guy-texture "clojure-gl/guy.png")
 
-(defn draw-tri [p1 p2 p3]
+(defn draw-tri [p1 p2 p3 t1 t2 t3]
+  (GL11/glBindTexture GL11/GL_TEXTURE_2D (*texture-cache* guy-texture))
   (GL11/glBegin GL11/GL_TRIANGLES)
+  (GL11/glTexCoord2f (t1 0) (t1 1))
+  (GL11/glVertex2f (p1 0) (p1 1))
+  (GL11/glTexCoord2f (t2 0) (t2 1))
+  (GL11/glVertex2f (p2 0) (p2 1))
+  (GL11/glTexCoord2f (t3 0) (t3 1))
+  (GL11/glVertex2f (p3 0) (p3 1))
+  (GL11/glEnd)
+
+  (GL11/glBegin GL11/GL_LINE_LOOP)
   (GL11/glVertex2f (p1 0) (p1 1))
   (GL11/glVertex2f (p2 0) (p2 1))
   (GL11/glVertex2f (p3 0) (p3 1))
@@ -29,7 +41,8 @@
   (GL11/glTranslatef (* 0.5 *aspect-ratio*) 0.5 0.0)
   (GL11/glScalef 0.5 0.5 0.5)
   (GL11/glRotatef (mod (/ game-state 100) 360) 0.0 0.0 1.0)
-  (draw-tri [0.0 0.5] [-0.5 -0.5] [0.5 -0.5])
+  (draw-tri [0.0 0.5] [-0.5 -0.5] [0.5 -0.5]
+            [0.4 0.7] [0.0 0.0] [0.8 0.0])
   (GL11/glPopMatrix)
   (Display/update)
   (Display/sync 60)
@@ -50,7 +63,9 @@
   (GL11/glMatrixMode GL11/GL_PROJECTION)
   (GL11/glLoadIdentity)
   (GL11/glOrtho 0 *aspect-ratio* 0 1 1 -1)
-  (GL11/glMatrixMode GL11/GL_MODELVIEW))
+  (GL11/glMatrixMode GL11/GL_MODELVIEW)
+  (GL11/glEnable GL11/GL_TEXTURE_2D)
+  (GL11/glDisable GL11/GL_DEPTH_TEST))
 
 (defn run []
   (Display/setTitle "Hello World")
@@ -59,7 +74,8 @@
   (try
     (binding [*width* (Display/getWidth)
               *height* (Display/getHeight)
-              *aspect-ratio* (/ (Display/getWidth) (Display/getHeight))]
+              *aspect-ratio* (/ (Display/getWidth) (Display/getHeight))
+              *texture-cache* (preload-textures nil guy-texture)]
       (init-gl)
       (game-loop))
 
---
+++ src/clojure_gl/texture.clj
@@ -0,0 +1,111 @@
+(ns clojure-gl.texture
+  (:import (java.awt Color Graphics)
+           (java.awt.color ColorSpace)
+           (java.awt.image ColorModel ComponentColorModel DataBuffer Raster BufferedImage)
+           (javax.imageio ImageIO)
+           (org.lwjgl.opengl GL11)
+           (java.io BufferedInputStream)
+           (java.nio ByteBuffer ByteOrder IntBuffer)
+           (java.util Hashtable)))
+
+(defn create-int-buffer [sz]
+  (let [bb (ByteBuffer/allocateDirect (* 4 sz))]
+    (.order bb (ByteOrder/nativeOrder))
+    (.asIntBuffer bb)))
+
+(defn create-texture-id []
+  (let [ib (create-int-buffer 1)]
+    (GL11/glGenTextures ib)
+    (.get ib 0)))
+
+(defn resource-as-stream [ref]
+  (let [thr (Thread/currentThread)
+        loader (.getContextClassLoader thr)]
+    (.getResourceAsStream loader ref)))
+
+(defn greater-power-of-two [n]
+  (loop [x 2]
+    (if (< x n)
+      (recur (* x 2))
+      x)))
+
+(defn has-alpha [img]
+  (.. img (getColorModel) (hasAlpha)))
+
+(defn alpha-color-model []
+  (ComponentColorModel. (ColorSpace/getInstance ColorSpace/CS_sRGB)
+                        (int-array [8 8 8 8])
+                        true
+                        false
+                        ComponentColorModel/TRANSLUCENT
+                        DataBuffer/TYPE_BYTE))
+
+(defn color-model []
+  (ComponentColorModel. (ColorSpace/getInstance ColorSpace/CS_sRGB)
+                        (int-array [8 8 8 0])
+                        false
+                        false
+                        ComponentColorModel/OPAQUE
+                        DataBuffer/TYPE_BYTE))
+
+(defn convert-texture-data [img]
+  (let [width (greater-power-of-two (.getWidth img))
+        height (greater-power-of-two (.getHeight img))
+        raster (if (has-alpha img)
+                 (Raster/createInterleavedRaster DataBuffer/TYPE_BYTE width height 4 nil)
+                 (Raster/createInterleavedRaster DataBuffer/TYPE_BYTE width height 3 nil))
+        tex-image (if (has-alpha img)
+                    (BufferedImage. (alpha-color-model) raster false (Hashtable.))
+                    (BufferedImage. (color-model) raster false (Hashtable.)))
+        tex-g (.getGraphics tex-image)]
+
+    (doto tex-g
+      (.setColor (Color. 0 0 0 0))
+      (.fillRect 0 0 width height)
+      (.drawImage img 0 0 nil))
+
+    (let [data (.. tex-image (getRaster) (getDataBuffer) (getData))
+          buffer (ByteBuffer/allocateDirect (count data))]
+      (doto buffer
+        (.order (ByteOrder/nativeOrder))
+        (.put data 0 (count data))
+        (.flip))
+      buffer)))
+
+(defn load-image [ref]
+  (if-let [resource (resource-as-stream ref)]
+    (ImageIO/read (BufferedInputStream. resource))))
+
+(defn load-texture [resource]
+  (if-let [image (load-image resource)]
+    (let [texid (create-texture-id)
+          width (greater-power-of-two (.getWidth image))
+          height (greater-power-of-two (.getHeight image))
+          bytes (convert-texture-data image)]
+      (GL11/glBindTexture GL11/GL_TEXTURE_2D texid)
+      (GL11/glTexParameteri GL11/GL_TEXTURE_2D GL11/GL_TEXTURE_MIN_FILTER GL11/GL_LINEAR)
+      (GL11/glTexParameteri GL11/GL_TEXTURE_2D GL11/GL_TEXTURE_MAG_FILTER GL11/GL_LINEAR)
+      (GL11/glTexImage2D GL11/GL_TEXTURE_2D
+                         0
+                         GL11/GL_RGBA
+                         width height
+                         0
+                         (if (has-alpha image) GL11/GL_RGBA GL11/GL_RGB)
+                         GL11/GL_UNSIGNED_BYTE
+                         bytes)
+      texid)))
+
+(defn get-texture [resource texture-cache]
+  (let [texture-cache (or texture-cache {})]
+    (if-let [texture (texture-cache resource)]
+      [texture texture-cache]
+      (let [texture (load-texture resource)]
+        [texture (conj texture-cache {resource texture})]))))
+
+(defn preload-textures [cache & names]
+  (loop [cache cache
+         names names]
+    (if (first names)
+      (let [[_ cache] (get-texture (first names) cache)]
+        (recur cache (rest names)))
+      cache)))
